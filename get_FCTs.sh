ff4aa5bba3bad74a3a29f15712a61a1fe145cad5
Ajoukov <aapteryx@gmail.com>
2025-05-02 19:36:01 -0400

    multiprocess FCT collection

--- get_FCTs.sh
+++ get_FCTs.sh
@@ -10,7 +10,14 @@ for transport in "${transports[@]}"; do
     for load in "${loads[@]}"; do
       output_file="fct_w${workload}_t${transport}_l${load}.log"
       echo "Running workload=${workload}, transport=${transport}, load=${load}"      
-      ns-3-dev/ns3 run tcp-ppt-example -- --workload=${workload} --transport=${transport} --sim_time=${sim_time} --load=${load} > ${output_file}
+      stdbuf -oL \
+      ns-3-dev/ns3 run tcp-ppt-example -- \
+          --workload=${workload} \
+          --transport=${transport} \
+          --sim_time=${sim_time} \
+           --load=${load} \
+           > ${output_file} 2> ${output_file} &
+          # --load=${load}
     done
   done
 done
